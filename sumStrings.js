28792f33ad77be69898a8ebc156bad090925e7d9
Karla Moore <15236034+KMoore7@users.noreply.github.com>
2022-08-24 18:57:35 -0400

    Sum two numbers and then convert them to a string

---
+++ sumStrings.js
@@ -0,0 +1,4 @@
+function sumStr(a,b) {  //declare function anmed sumStr with parameters (a,b)
+    return Number(a) + Number (b) +'' //return the sum of a+b as numbers and then turn into a string by adding ''
+    
+  }
